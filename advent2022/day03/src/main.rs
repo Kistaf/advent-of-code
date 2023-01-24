fb620fa172ede612aea36a88fe7020e530dbf829
stkea <stef240p@stud.kea.dk>
2023-01-24 15:31:11 +0100

    finished second challenge of advent2022 day03

--- advent2022/day03/src/main.rs
+++ advent2022/day03/src/main.rs
@@ -3,17 +3,43 @@ use std::fs;
 fn main() {
     let path: &str = "data.txt";
     let data = fs::read_to_string(path).expect(&format!("Failed to load {}", path));
-    let total: usize = data.split("\n").map(|line| find_common_score(line).unwrap_or(0)).sum();
-    println!("The total score is: {}", total)
+    let priority: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let mut lines: Vec<&str> = Vec::new(); // used for part 2
+    let total: usize = data
+        .split("\n")
+        .map(|line| -> usize {
+            lines.push(line); // part 2 preparation
+            match find_common(line) {
+                Some(c) => priority.find(c).unwrap_or(0) + 1,
+                None => 0,
+            }
+        })
+        .sum();
+
+    // Part 2
+    let mut sum: i32 = 0;
+    for i in (2..lines.len() - 1).step_by(3) {
+        for letter in lines[i].chars() {
+            if lines[i - 1].contains(letter) && lines[i - 2].contains(letter) {
+                sum += match priority.find(letter) {
+                    Some(r) => r + 1,
+                    None => 0,
+                } as i32;
+                break;
+            }
+        }
+    }
+
+    // Results
+    println!("The total score is: {}", total);
+    println!("Total sum: {}", sum);
 }
 
-fn find_common_score(s: &str) -> Option<usize> {
+fn find_common(s: &str) -> Option<char> {
     let (l, r) = s.split_at(s.len() / 2);
     for letter in l.chars() {
         if r.contains(letter) {
-            let priority: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            let score: usize = priority.find(letter)? + 1;
-            return Some(score);
+            return Some(letter);
         }
     }
     return None;
